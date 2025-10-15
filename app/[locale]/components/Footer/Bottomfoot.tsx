991a1b4e2fcec5661e0a7b4c1a35813f57b0d043
krisho-code <kristo.dominik.levente@gmail.com>
2025-10-15 14:41:40 +0200

    edited bottom footer

--- app/[locale]/components/Footer/Bottomfoot.tsx
+++ app/[locale]/components/Footer/Bottomfoot.tsx
@@ -8,22 +8,14 @@ const Bottomfoot = () => {
 
   return (
     <div className="bottomfoot flex items-center justify-center h-24 md:h-12 border-t-2 border-gray-200">
-      <div className="container flex flex-col gap-2 md:flex-row items-center justify-between px-8">
-        <p className="text-regular-nomral">{t("footer.copyright")}</p>
-        <ul className="links flex items-center gap-4">
+      <div className="container flex flex-col items-start gap-2 md:flex-row md:items-center justify-between px-8">
+        <p className="text-regular-nomral order-2 md:order-1">
+          {t("footer.copyright")}
+        </p>
+        <ul className="links flex items-center gap-4 order-1 md:order-2">
           <li>
-            <a href="#" className="text-regular-nomral">
-              {t("footer.links.faq")}
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-regular-nomral">
+            <a href="#" className="text-regular-nomral underline">
               {t("footer.links.cookies")}
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-regular-nomral">
-              {t("footer.links.other")}
             </a>
           </li>
         </ul>
