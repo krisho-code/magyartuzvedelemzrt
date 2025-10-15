b344e2e603dc14f3a14acdfdd392a79174db0812
krisho-code <kristo.dominik.levente@gmail.com>
2025-10-15 15:25:11 +0200

    added learn more to the homeserviceinfoscontent

--- app/[locale]/components/Home/HomeServicesInfosContent.tsx
+++ app/[locale]/components/Home/HomeServicesInfosContent.tsx
@@ -10,9 +10,13 @@ const ServiceInfo: React.FC<ServiceInfoProps> = ({ title, link }) => {
   return (
     <div className="service-info flex items-start gap-8">
       <img src="/icons/fire-icon-secondary.svg" alt="" />
-      <div className="content flex flex-col gap-4">
-        <Link href={`${link}`}>
+      <div className="content flex">
+        <Link href={`${link}`} className="flex flex-col gap-2 md:gap-4">
           <h3 className="text-h3">{title}</h3>
+          <p className="text-regular-normal underline flex gap-4 items-center">
+            Tudjon meg többet{" "}
+            <img src="/icons/arrow-icon.svg" className="w-2" />
+          </p>
         </Link>
         {/* <p className="text-medium-normal">{description}</p>
         <Link href={`${link}`} className="text-regular-normal flex gap-2">
