37492086400162e541f532f52f9bec219e35d032
krisho-code <kristo.dominik.levente@gmail.com>
2025-12-11 14:23:30 +0100

    language problem fix

--- app/[locale]/components/Global Components/AlterLogo.tsx
+++ app/[locale]/components/Global Components/AlterLogo.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { useCurrentLocale } from "@/locales/client";
 
 interface LogoProps {
   mobileToggle?: () => void;
 }
 
 const Logo: React.FC<LogoProps> = ({ mobileToggle }) => {
+  const locale = useCurrentLocale();
+
   return (
-    <Link href="/" className="logo max-w-40" onClick={mobileToggle}>
+    <Link href={`/${locale}`} className="logo max-w-40" onClick={mobileToggle}>
       <img src="/alter-logo.svg" alt="Magyar Tűzvédelem Zrt." />
     </Link>
   );
